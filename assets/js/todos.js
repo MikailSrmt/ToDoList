e34f0b5fada3fbed888b6bd50c2f5b88f9aaffb7
MikailSeremet <mikail.seremet@gmail.com>
2017-06-03 17:57:13 +0300

    Styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -15,7 +15,11 @@ $("input[type='text']").on("keypress",function(key){
     if(key.which === 13){
         var newToDo= $(this).val();
         $(this).val("");
-        $("ul").append("<li>" + "<span>X</span>"+ " " + newToDo + "</li>");
+        $("ul").append("<li>" + "<span><i class=\"fa fa-times\" aria-hidden=\"true\"></i></span>"+ " " + newToDo + "</li>");
     }
     
+});
+
+$(".fa-pencil-square-o").on("click", function(){
+    $("input[type='text']").slideToggle();
 });
